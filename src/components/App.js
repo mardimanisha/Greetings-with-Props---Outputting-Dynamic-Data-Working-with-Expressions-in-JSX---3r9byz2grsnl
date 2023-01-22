3e01a565e6774d2709947265abb21927d4f5c429
Manisha Mardi <118474184+mardimanisha@users.noreply.github.com>
2023-01-22 22:32:49 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,9 @@ import '../styles/App.css';
 import Welcome from './Welcome';
 
 const App = () => {
+  let name = "Anand Dubey";
   return (
-    <Welcome name="" />
+    <Welcome name={name} />
   )
 }
 
